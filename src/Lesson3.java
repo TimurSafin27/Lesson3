ae6409202aa9ca5805ff042b6f67d6b9b9a9fbf4
TimurSafin27 <108293724+TimurSafin27@users.noreply.github.com>
2022-08-05 10:03:53 +0500

    Update Lesson3.java
    12

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,5 +1,41 @@
-public class Lesson3 {
-    public static void main (String[] args){
-
+public class Lesson3 
+{
+    public static void main (String[] args)
+	{
+		Object[] ArrayList = new Object[10];
+		Object[] ArrayListWeighty = new Object[ArrayList.size];
+		int SortWeight;
+		int j=0;
+		for (int i=0; i<ArrayList.size; i++)
+			{
+				ArrayList[i] = new Box();
+			}
+		for (int i=0; i<ArrayList.size; i++)
+			{
+				if(ArrayList[i].Weight > SortWeight);
+					{
+						ArrayListWeighty[j]=ArrayList[i];
+						j++;
+						ArrayList.remove(i);
+					}
+			}
+		System.Out.Println ("Box весом не более 300 гр.:");
+		for (int i=0; i<ArrayList.size; i++)
+			{
+				System.Out.Println ("Box №" + i+ " весом "+ ArrayList[i].Weight + "гр.");
+			}
+		System.Out.Println ("Box весом более 300 гр.:");
+		for (int i=0; i<ArrayListWeighty.size; i++)
+			{
+				System.Out.Println ("Box №" + i+ " весом "+ ArrayListWeighty[i].Weight + "гр.");
+			}
     }
+}
+
+
+public class Box{
+	int Weight = random.nextint(500);//вес в гр.
+    int Height = random.nextint(100);//высота
+	int Width = random.nextint(100);;//ширина
+	int Depth = random.nextint(100);;//глубина
 }
