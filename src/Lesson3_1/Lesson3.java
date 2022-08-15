c1aaa2a628a834661626779181b19152d91340e6
TimurSafin27 <108293724+TimurSafin27@users.noreply.github.com>
2022-08-15 15:39:35 +0500

    Update Lesson3.java

--- src/Lesson3_1/Lesson3.java
+++ src/Lesson3_1/Lesson3.java
@@ -11,57 +11,56 @@ public class Lesson3
         for (int i=0; i<ArrayList.length; i++)
         {
             Random rn = new Random();
-            int Weight = rn.nextInt(500) + 1;//вес в гр.
-            int Height = rn.nextInt(500) + 1;//высота
-            int Width = rn.nextInt(500) + 1;;//ширина
-            int Depth = rn.nextInt(500) + 1;;//глубина
-            //System.out.println ("вес:" + Weight + " длина:" + Height + " ирина"  + Width);
+            int Weight = rn.nextInt(500) + 1;//ГўГҐГ± Гў ГЈГ°.
+            int Height = rn.nextInt(500) + 1;//ГўГ»Г±Г®ГІГ 
+            int Width = rn.nextInt(500) + 1;;//ГёГЁГ°ГЁГ­Г 
+            int Depth = rn.nextInt(500) + 1;;//ГЈГ«ГіГЎГЁГ­Г 
+            //System.out.println ("ГўГҐГ±:" + Weight + " Г¤Г«ГЁГ­Г :" + Height + " ГЁГ°ГЁГ­Г "  + Width);
             ArrayList[i] = new Box(Weight, Weight, Width,Depth);
-            System.out.println (i + " - "+ ArrayList[i].Weight + "гр. ");
+            System.out.println (i + " - "+ ArrayList[i].Weight + "ГЈГ°. ");
         }
-        System.out.println ("Цикл:");
+        System.out.println ("Г–ГЁГЄГ«:");
         for (int i=0; i<ArrayList.length; i++)
         {
             if(ArrayList[i].Weight > SortWeight)
             {
-                System.out.println ("Box №" + i +"    весом "+ArrayList[i].Weight + "гр. больше чем"+ SortWeight);
+                System.out.println ("Box В№" + i +"    ГўГҐГ±Г®Г¬ "+ArrayList[i].Weight + "ГЈГ°. ГЎГ®Г«ГјГёГҐ Г·ГҐГ¬"+ SortWeight);
                 ArrayListWeighty[j]=ArrayList[i];
-                System.out.println ("Box №" + j +"    весом "+ArrayListWeighty[j].Weight + "гр.");
+                System.out.println ("Box В№" + j +"    ГўГҐГ±Г®Г¬ "+ArrayListWeighty[j].Weight + "ГЈГ°.");
                 j++;
-                System.out.println ("Считаем:");
-                for (int k=i+1; k<ArrayList.length; k++)
+                System.out.println ("Г‘Г·ГЁГІГ ГҐГ¬:");
+                for (int q=i; q<ArrayList.length; q++)
                 {
-                    int q=k-1;
-                    System.out.println ("До смещения" + q +"    весом "+ArrayList[k-1].Weight + "гр.");
-                    System.out.println ("Следующий" + k +"    весом "+ArrayList[k].Weight + "гр.");
-                    if(ArrayList[k]==null)
+                    int k=q+1;
+                    System.out.println ("Г„Г® Г±Г¬ГҐГ№ГҐГ­ГЁГї" + q +"    ГўГҐГ±Г®Г¬ "+ArrayList[q].Weight + "ГЈГ°.");
+                    System.out.println ("Г‘Г«ГҐГ¤ГіГѕГ№ГЁГ©" + k +"    ГўГҐГ±Г®Г¬ "+ArrayList[k].Weight + "ГЈГ°.");
+                    if (q=ArrayList.length)
+		    {
+			ArrayList[q]=null
+		    }
+		    else if(ArrayList[k]==null)
                     {
-                        System.out.println ("Нуль:");
-                        ArrayList[k-1]=null;
+                        System.out.println ("ГЌГіГ«Гј:");
+                        ArrayList[q]=null;
                     }
-                    /*else if(k==ArrayList.length-1)
-                    {
-                        ArrayList[k-1]=null;
-                        System.out.println ("Обнулил 9" + q);
-                    }*/
                     else
                     {
-                        ArrayList[k-1]=ArrayList[k];
-                        System.out.println ("После смещения" + q +"    весом "+ArrayList[k-1].Weight + "гр.");
+                        ArrayList[q]=ArrayList[k];
+                        System.out.println ("ГЏГ®Г±Г«ГҐ Г±Г¬ГҐГ№ГҐГ­ГЁГї" + q +"    ГўГҐГ±Г®Г¬ "+ArrayList[q].Weight + "ГЈГ°.");
                     }
                 }
             }
         }
-        System.out.println ("Box весом не более 300 гр.:");
+        System.out.println ("Box ГўГҐГ±Г®Г¬ Г­ГҐ ГЎГ®Г«ГҐГҐ 300 ГЈГ°.:");
         for (int i=0; i<ArrayList.length; i++)
         {
             int k=i+1;
-            System.out.println ("Box №" + k + " весом "+ ArrayList[i].Weight + "гр.");
+            System.out.println ("Box В№" + k + " ГўГҐГ±Г®Г¬ "+ ArrayList[i].Weight + "ГЈГ°.");
         }
-		System.out.println ("Box весом более 300 гр.:");
+		System.out.println ("Box ГўГҐГ±Г®Г¬ ГЎГ®Г«ГҐГҐ 300 ГЈГ°.:");
 		/*for (int i=0; i<ArrayListWeighty.length; i++)
 			{
-				System.out.println ("Box №" + i+ " весом "+ ArrayListWeighty[i].Weight + "гр.");
+				System.out.println ("Box В№" + i+ " ГўГҐГ±Г®Г¬ "+ ArrayListWeighty[i].Weight + "ГЈГ°.");
 			}*/
     }
 }
